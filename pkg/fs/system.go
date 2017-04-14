828865a4d00726193f6b7f9f91fe859449808f89
PI-Victor <palade.ionut@gmail.com>
2017-04-14 22:31:53 +0200

    improve and split main configuration

--- pkg/fs/system.go
+++ pkg/fs/system.go
@@ -2,24 +2,42 @@ package fs
 
 import "github.com/Sirupsen/logrus"
 
+// IRCSettings holds information about one or more IRC servers that the bot
+// could join.
+type IRCSettings struct {
+	IRCServers []IRCServer `json:"ircServers"`
+}
+
+// IRCServer holds information regarding one or more IRC servers that the bot
+// should connect to.
+type IRCServer struct {
+	ServerName string   `json:"serverName"`
+	Nick       string   `json:"nick"`
+	Channels   []string `json:"channels"`
+}
+
+// JenkinsSettings holds the information about one or more Jenkins servers that
+// the bot should send or retrieve information from.
+type JenkinsSettings struct {
+	JenkinsURL   string `json:"jenkinsURL"`
+	JenkinsUser  string `json:"jenkinsUser"`
+	JenkinsToken string `json:"jenkinsToken"`
+}
+
 // Config holds the configuration options for the application.
 type Config struct {
 	DebugLevel logrus.Level `json:"debugLevel"`
 
+	IRCServers []IRCSettings `json:"ircServers"`
+
+	JenkinsServers []JenkinsSettings `json:"jenkinsServers"`
+
 	GitHubUser  string `json:"githubUser"`
 	GitHubToken string `json:"gitHubToken"`
-
-	JenkinsURL   string `json:"jenkinsURL"`
-	JenkinsUser  string `json:"jenkinsUser"`
-	JenkinsToken string `json:"jenkinsToken"`
-
-	IRCUser   string `json:"ircUser"`
-	IRCServer string `json:"ircServer"`
+	// A list of URLs that the bot can ignore.
+	GitHubIgnoreList []string `json:"gitHubIgnoreList"`
 
 	TravisToken string `json:"travisToken"`
-
-	// URL List of repositories to watch.
-	WatchList []string `json:"watchList"`
 }
 
 // NewConfig returns a new empty config instance.
@@ -29,8 +47,14 @@ func NewConfig() *Config {
 
 // NewDefaultConfig is the default config that is used to generate a new
 // config.json file
-func NewDefaultConfig() *Config {
+func newDefaultConfig() *Config {
 	return &Config{
 		DebugLevel: logrus.InfoLevel,
 	}
+}
+
+// CreateDefaultCfg creates a default config.json in the current working
+// directory.
+func CreateDefaultCfg() error {
+	return nil
 }
